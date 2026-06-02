0d8ec42bb92066b336758336edaca032987a4c9b
newhope8397 <nhope8397@gmail.com>
2026-06-02 05:54:31 +0530

    Implement file management functions in file-engine.js

--- js/core/file-engine.js
+++ js/core/file-engine.js
@@ -1,1 +1,29 @@
+// core/file-engine.js
 
+let currentFile = null;
+let fileType = null;
+let fileName = null;
+
+export function setFile(file, type, name) {
+    currentFile = file;
+    fileType = type;
+    fileName = name;
+}
+
+export function getFile() {
+    return currentFile;
+}
+
+export function getFileType() {
+    return fileType;
+}
+
+export function getFileName() {
+    return fileName;
+}
+
+export function clearFile() {
+    currentFile = null;
+    fileType = null;
+    fileName = null;
+}
