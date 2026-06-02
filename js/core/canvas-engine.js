fb98e5d0fcddaf0edcee7aa8c94c62329e4e8b84
newhope8397 <nhope8397@gmail.com>
2026-06-02 07:19:54 +0530

    Add canvas manipulation functions

--- js/core/canvas-engine.js
+++ js/core/canvas-engine.js
@@ -1,1 +1,53 @@
+// js/core/canvas-engine.js
 
+import { getEditor } from "../editor-core.js";
+
+export function clearCanvas() {
+
+    const { canvas, ctx } =
+        getEditor();
+
+    ctx.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+}
+
+export function drawImage(img) {
+
+    const { canvas, ctx } =
+        getEditor();
+
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    clearCanvas();
+
+    ctx.drawImage(
+        img,
+        0,
+        0
+    );
+}
+
+export function resizeCanvas(
+    width,
+    height
+) {
+
+    const { canvas } =
+        getEditor();
+
+    canvas.width = width;
+    canvas.height = height;
+}
+
+export function canvasToDataURL() {
+
+    const { canvas } =
+        getEditor();
+
+    return canvas.toDataURL();
+}
