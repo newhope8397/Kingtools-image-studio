f92374a987b1345fd98043e8109b18c3ca002f64
newhope8397 <nhope8397@gmail.com>
2026-06-03 17:11:07 +0530

    Create editor-state.js for managing editor state

---
+++ js/core/editor-state.js
@@ -0,0 +1,17 @@
+// core/editor-state.js
+
+export const state = {
+    canvas: null,
+    ctx: null,
+
+    image: null,
+
+    history: [],
+    historyIndex: -1,
+
+    currentFile: null,
+    fileType: null,
+    fileName: null,
+
+    activeTool: null
+};
