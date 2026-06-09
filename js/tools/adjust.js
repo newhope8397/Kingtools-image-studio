eecf6678313e4d5b4306e823003de53c628c59ef
newhope8397 <nhope8397@gmail.com>
2026-06-09 23:44:58 +0530

    Update adjust.js

--- js/tools/adjust.js
+++ js/tools/adjust.js
@@ -1,34 +1,92 @@
-import { getEditor } from '../editor-core.js';
-import { saveHistory }
-from "../core/history-engine.js";
+import { getEditor } from "../editor-core.js";
+import { saveHistory } from "../core/history-engine.js";
+
+let originalImageData = null;
 
 export function showAdjustPanel() {
-    const panel = document.getElementById('tool-panel');
+
+    const {
+        canvas,
+        ctx
+    } = getEditor();
+
+    originalImageData =
+        ctx.getImageData(
+            0,
+            0,
+            canvas.width,
+            canvas.height
+        );
+
+    const panel =
+        document.getElementById(
+            "tool-panel"
+        );
 
     panel.innerHTML = `
-        <div>Brightness</div>
-        <input type="range" min="50" max="150" value="100" oninput="adjustBrightness(this.value)">
+        <div class="p-4">
+
+            <div>Brightness</div>
+
+            <input
+                type="range"
+                min="50"
+                max="150"
+                value="100"
+                oninput="adjustBrightness(this.value)"
+            >
+
+            <button
+                onclick="applyBrightness()"
+            >
+                Apply
+            </button>
+
+        </div>
     `;
 }
 
 window.adjustBrightness = (value) => {
-    const { canvas, ctx } = getEditor();
 
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data;
+    const {
+        canvas,
+        ctx
+    } = getEditor();
 
-    const factor = value / 100;
+    const imageData =
+        new ImageData(
+            new Uint8ClampedArray(
+                originalImageData.data
+            ),
+            originalImageData.width,
+            originalImageData.height
+        );
 
-    for (let i = 0; i < data.length; i += 4) {
+    const data =
+        imageData.data;
+
+    const factor =
+        value / 100;
+
+    for (
+        let i = 0;
+        i < data.length;
+        i += 4
+    ) {
+
         data[i] *= factor;
-        data[i+1] *= factor;
-        data[i+2] *= factor;
-    }
-    window.applyBrightness = () => {
-   saveHistory();
+        data[i + 1] *= factor;
+        data[i + 2] *= factor;
     }
 
-    ctx.putImageData(imageData, 0, 0);
+    ctx.putImageData(
+        imageData,
+        0,
+        0
+    );
 };
 
-window.showAdjustPanel = showAdjustPanel;
+window.applyBrightness = () => {
+
+    saveHistory();
+};
