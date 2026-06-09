aa2da5538d25de0a26b80a1cda4f36156cbb3f1b
newhope8397 <nhope8397@gmail.com>
2026-06-09 23:42:46 +0530

    Update tool-engine.js

--- js/core/tool-engine.js
+++ js/core/tool-engine.js
@@ -1,1 +1,27 @@
+// js/core/tool-engine.js
 
+let cleanupCurrentTool = null;
+
+export function activateTool(initTool) {
+
+    // cleanup previous tool
+    if (cleanupCurrentTool) {
+
+        cleanupCurrentTool();
+
+        cleanupCurrentTool = null;
+    }
+
+    // start new tool
+    cleanupCurrentTool = initTool();
+}
+
+export function deactivateTool() {
+
+    if (cleanupCurrentTool) {
+
+        cleanupCurrentTool();
+
+        cleanupCurrentTool = null;
+    }
+}
