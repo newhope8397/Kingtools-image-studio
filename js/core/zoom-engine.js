f0f685dfe0b50c3727ce4204914ab5984b6d327a
newhope8397 <nhope8397@gmail.com>
2026-06-10 14:37:49 +0530

    Create zoom-engine.js

---
+++ js/core/zoom-engine.js
@@ -0,0 +1,32 @@
+// core/zoom-engine.js
+
+import { state } from "./editor-state.js";
+
+export function setZoom(value) {
+
+    state.zoom = value;
+}
+
+export function zoomIn() {
+
+    state.zoom += 0.1;
+
+    return state.zoom;
+}
+
+export function zoomOut() {
+
+    state.zoom = Math.max(
+        0.1,
+        state.zoom - 0.1
+    );
+
+    return state.zoom;
+}
+
+export function resetZoom() {
+
+    state.zoom = 1;
+
+    return state.zoom;
+}
