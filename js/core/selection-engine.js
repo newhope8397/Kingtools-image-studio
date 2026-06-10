a3dd051e4e100a732e422c1d0707a78714e72b62
newhope8397 <nhope8397@gmail.com>
2026-06-10 13:47:35 +0530

    Update selection-engine.js

--- js/core/selection-engine.js
+++ js/core/selection-engine.js
@@ -1,1 +1,30 @@
+// js/core/selection-engine.js
 
+let selection = null;
+
+export function createSelection(
+    x,
+    y,
+    w,
+    h
+) {
+
+    selection = {
+        x,
+        y,
+        w,
+        h
+    };
+}
+
+export function getSelection() {
+    return selection;
+}
+
+export function clearSelection() {
+    selection = null;
+}
+
+export function hasSelection() {
+    return selection !== null;
+}
