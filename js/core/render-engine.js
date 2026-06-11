0a6520bcd1c91345216ce6b37646f73af47a1833
newhope8397 <nhope8397@gmail.com>
2026-06-11 12:48:11 +0530

    Update render-engine.js

--- js/core/render-engine.js
+++ js/core/render-engine.js
@@ -1,1 +1,47 @@
+// core/render-engine.js//
 
+import { state } from "./editor-state.js";
+
+export function render() {
+
+    if (
+        !state.canvas ||
+        !state.ctx ||
+        !state.image
+    ) return;
+
+    const ctx = state.ctx;
+    const canvas = state.canvas;
+
+    ctx.clearRect(
+        0,
+        0,
+        canvas.width,
+        canvas.height
+    );
+
+    ctx.save();
+
+    ctx.translate(
+        canvas.width / 2,
+        canvas.height / 2
+    );
+
+    ctx.scale(
+        state.zoom,
+        state.zoom
+    );
+
+    ctx.rotate(
+        state.rotation *
+        Math.PI / 180
+    );
+
+    ctx.drawImage(
+        state.image,
+        -state.image.width / 2,
+        -state.image.height / 2
+    );
+
+    ctx.restore();
+}
