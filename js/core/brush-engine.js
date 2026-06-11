a54ddcb623f8cdd60b0629d6972529f425b685d7
newhope8397 <nhope8397@gmail.com>
2026-06-11 12:49:49 +0530

    Update brush-engine.js

--- js/core/brush-engine.js
+++ js/core/brush-engine.js
@@ -1,1 +1,36 @@
+// core/brush-engine.js //
 
+
+import { state } from "./editor-state.js";
+
+let brushSize = 20;
+
+export function setBrushSize(size) {
+
+    brushSize = size;
+}
+
+export function getBrushSize() {
+
+    return brushSize;
+}
+
+export function drawBrush(
+    x,
+    y
+) {
+
+    if (!state.ctx) return;
+
+    state.ctx.beginPath();
+
+    state.ctx.arc(
+        x,
+        y,
+        brushSize / 2,
+        0,
+        Math.PI * 2
+    );
+
+    state.ctx.fill();
+}
