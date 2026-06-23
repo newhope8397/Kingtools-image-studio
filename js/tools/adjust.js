7d3b22accf6b89927269e27fcf6b1597b212b753
newhope8397 <nhope8397@gmail.com>
2026-06-23 10:50:05 +0530

    Update adjust.js

--- js/tools/adjust.js
+++ js/tools/adjust.js
@@ -1,4 +1,5 @@
 import { getEditor } from "../editor-core.js";
+import { syncStateImage } from "../core/canvas-engine.js";
 import { saveHistory } from "../core/history-engine.js";
 import { requireImage } from "../core/guard-engine.js";
 import { openPanel } from "../core/panel-engine.js";
