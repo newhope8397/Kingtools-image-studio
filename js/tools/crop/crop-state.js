141ceab044cb57d03446efbd1ad09f106273adb4
newhope8397 <nhope8397@gmail.com>
2026-08-03 23:02:19 +0530

    Create crop-state.js

---
+++ js/tools/crop/crop-state.js
@@ -0,0 +1,23 @@
+export const cropState = {
+
+    active: false,
+
+    dragging: false,
+
+    resizing: false,
+
+    handle: null,
+
+    moveMode: false,
+
+    x: 0,
+    y: 0,
+
+    width: 0,
+    height: 0,
+
+    startX: 0,
+    startY: 0,
+
+    aspect: "free"
+};
