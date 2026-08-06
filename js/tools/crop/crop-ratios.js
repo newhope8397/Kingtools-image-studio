803ed4460d5a36228acc196245db34ca7e4cd679
newhope8397 <nhope8397@gmail.com>
2026-08-06 17:18:50 +0530

    Update crop-ratios.js

--- js/tools/crop/crop-ratios.js
+++ js/tools/crop/crop-ratios.js
@@ -1,1 +1,76 @@
+// js/tools/crop/crop-ratios.js
 
+import { cropState }
+from "./crop-state.js";
+
+export const RATIOS = {
+
+    free:null,
+
+    square:1,
+
+    portrait:4/5,
+
+    landscape:16/9,
+
+    instagram:1,
+
+    story:9/16,
+
+    classic:4/3
+
+};
+
+export function setAspect(name){
+
+    cropState.aspect =
+        name;
+
+}
+
+export function getAspect(){
+
+    return cropState.aspect;
+
+}
+
+export function applyAspect(
+    width,
+    height
+){
+
+    if(
+        cropState.aspect==="free"
+    ){
+
+        return{
+            width,
+            height
+        };
+
+    }
+
+    const ratio =
+        RATIOS[
+            cropState.aspect
+        ];
+
+    if(!ratio){
+
+        return{
+            width,
+            height
+        };
+
+    }
+
+    return{
+
+        width,
+
+        height:
+        width/ratio
+
+    };
+
+}
