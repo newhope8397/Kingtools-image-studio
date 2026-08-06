1cc8d456e6ef26ad7176a1d10706276c4e960baa
newhope8397 <nhope8397@gmail.com>
2026-08-06 15:34:43 +0530

    Update crop-handles.js

--- js/tools/crop/crop-handles.js
+++ js/tools/crop/crop-handles.js
@@ -1,3 +1,6 @@
+// js/tools/crop/crop-handles.js //  
+import { getEditor } from "../../editor-core.js";               
+
 export const HANDLE_SIZE = 14;
 
 export function getHandles(x, y, w, h) {
@@ -21,5 +24,35 @@ export function getHandles(x, y, w, h) {
         br:[x+w,y+h]
 
     };
+
+}
+
+export function drawHandles(x, y, w, h) {
+
+    const { ctx } = getEditor();
+
+    const handles =
+        getHandles(x, y, w, h);
+
+    Object.values(handles).forEach(([hx, hy]) => {
+
+        ctx.beginPath();
+
+        ctx.arc(
+            hx,
+            hy,
+            HANDLE_SIZE / 2,
+            0,
+            Math.PI * 2
+        );
+
+        ctx.fillStyle = "#ffffff";
+        ctx.fill();
+
+        ctx.strokeStyle = "#a78bfa";
+        ctx.lineWidth = 2;
+        ctx.stroke();
+
+    });
 
 }
